1ba507252c4c722ba0a66384b83485bc313293a4
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2024-09-26 19:29:48 +0200

    superHeroes

--- app/src/main/java/edu/iesam/dam2024/app/api/ApiClient.kt
+++ app/src/main/java/edu/iesam/dam2024/app/api/ApiClient.kt
@@ -4,11 +4,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiClient {
-    var retrofit = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
         .baseUrl("https://akabab.github.io/superhero-api/api/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    var apiService: ApiService = retrofit.create(ApiService::class.java)
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
 
 }
