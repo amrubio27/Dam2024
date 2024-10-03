19a3a8da7a6d33f92a9ef6926b0d6138678e76d3
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2024-10-03 17:45:29 +0200

    superHeroes updated DataRepository to update sharedPrefxml in case of missing data

--- app/src/main/java/edu/iesam/dam2024/features/superheroes/data/SuperHeroDataRepository.kt
+++ app/src/main/java/edu/iesam/dam2024/features/superheroes/data/SuperHeroDataRepository.kt
@@ -1,17 +1,35 @@
 package edu.iesam.dam2024.features.superheroes.data
 
+import edu.iesam.dam2024.features.superheroes.data.local.SuperHeroXmlLocalDataSource
 import edu.iesam.dam2024.features.superheroes.data.remote.SuperHeroRemoteDataSource
 import edu.iesam.dam2024.features.superheroes.domain.SuperHero
 import edu.iesam.dam2024.features.superheroes.domain.SuperHeroRepository
 
-class SuperHeroDataRepository(private val remoteDataSource: SuperHeroRemoteDataSource) :
+class SuperHeroDataRepository(
+    private val local: SuperHeroXmlLocalDataSource,
+    private val remote: SuperHeroRemoteDataSource
+) :
     SuperHeroRepository {
     override suspend fun getSuperHeroes(): List<SuperHero> {
-        return remoteDataSource.getSuperHeroes()
+        val superHeroesFromLocal = local.findAll()
+        return if (superHeroesFromLocal.isEmpty()) {
+            val superHeroesFromRemote = remote.getSuperHeroes()
+            local.saveAll(superHeroesFromRemote)
+            superHeroesFromRemote
+        } else {
+            superHeroesFromLocal
+        }
     }
 
     override suspend fun getSuperHeroById(id: String): SuperHero? {
-        return remoteDataSource.getSuperHeroById(id)
+        val superHeroFromLocal = local.findById(id)
+        return if (superHeroFromLocal == null) {
+            val superHeroFromRemote = remote.getSuperHeroById(id)
+            superHeroFromRemote?.let { local.save(it) }
+            superHeroFromRemote
+        } else {
+            superHeroFromLocal
+        }
     }
 }
 
