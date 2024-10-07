90e72a3a664da4902419fb164e92ac16c1eba103
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2024-10-07 20:42:17 +0200

    superhero basicauthor wip

--- app/src/main/java/edu/iesam/dam2024/app/api/ApiClient.kt
+++ app/src/main/java/edu/iesam/dam2024/app/api/ApiClient.kt
@@ -1,8 +1,10 @@
 package edu.iesam.dam2024.app.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/*
 class ApiClient {
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://akabab.github.io/superhero-api/api/")
@@ -11,4 +13,23 @@ class ApiClient {
 
     val apiService: ApiService = retrofit.create(ApiService::class.java)
 
+}*/
+
+class ApiClient {
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(
+            BasicAuthInterceptor(
+                "chema",
+                "769c36fd04d4af42665ec8f3d2271daccca629a14e8d8d94"
+            )
+        )
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://meceiot.usal.es/g/login/")
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
 }
